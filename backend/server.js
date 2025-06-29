72932933b6973213e492fd2c6c97c9d2f8b84071
jorge <jsantrichu67@gmail.com>
2025-06-28 20:19:37 -0400

    fix: explicitly handle CORS preflight requests

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
